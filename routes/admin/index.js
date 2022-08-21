f7a33cbe7d2d6d960b594cadc86138866202e30c
Admin <bskaya.mert@gmail.com>
2022-08-21 21:45:58 +0300

    Edit operation was added to admin/posts section

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const Category = require('../../models/Category')
 const router = express.Router()
 const Post = require('../../models/Post')
+const path = require('path')
 
 router.get('/', (req, res) => {
 
@@ -48,6 +49,32 @@ router.delete('/posts/:id', (req, res) => {
 
     Post.deleteOne({ _id: req.params.id }).lean().then(categories => {
         res.redirect('/admin/posts')
+    })
+
+})
+
+router.get('/posts/edit/:id', (req, res) => {
+    Post.findOne({_id: req.params.id}).lean().then(post => {
+        Category.find({}).lean().then(categories => {
+            res.render('admin/editPost', {post:post, categories:categories})
+        })
+    })
+})
+
+router.put('/posts/:id', (req, res) => {
+    let post_image = req.files.post_image
+    post_image.mv(path.resolve(__dirname, '../../public/img/postImages', post_image.name))
+
+    Post.findOneAndUpdate({_id: req.params.id}, {$set:{
+        title: req.body.title,
+        content: req.body.content,
+        date: req.body.date,
+        category: req.body.category,
+        post_image: `/img/postImages/${post_image.name}`
+    }}).lean().then(post => {
+        
+        res.redirect('/admin/posts')
+
     })
 
 })
